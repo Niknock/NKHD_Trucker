8a6ea6b9b0779617f64d7b5e55a5e82790479fc0
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-06-20 17:36:55 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,19 @@
 Config = {}
 
-Config.English = true
+Config.English = false
+
+Config.ShortRouteXPneeded = 1
+Config.MoneyShort = 5000
+Config.XPShort = 1
+
+Config.MiddleRouteXPneeded = 10
+Config.MoneyMiddle = 10000
+Config.XPMiddle = 2
+
+Config.LongRouteXPneeded = 80
+Config.MoneyLong = 25000
+Config.XPLong = 4
+
+Config.CayoRouteXPneeded = 150
+Config.MoneyCayo = 27500
+Config.XPCayo = 8
